6294230967e790a69ce0c2e713be2552eb9fa4a7
Srinivasan8888 <92676627+Srinivasan8888@users.noreply.github.com>
2022-07-27 12:14:56 +0530

    changed the mainactivity.xml

---
+++ app/src/main/java/com/example/gymkhana/User.java
@@ -0,0 +1,17 @@
+package com.example.gymkhana;
+
+public class User {
+    public String firstname;
+    public String lastname;
+    public String email;
+
+    public User(){
+    }
+
+    public User (String firstname, String lastname, String email ){
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+
+    }
+}
---
+++ app/src/main/java/com/example/gymkhana/loading_screen.java
@@ -0,0 +1,30 @@
+package com.example.gymkhana;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class loading_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loading_screen);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        handler.postDelayed(
+                new Runnable() {
+                    public void run() {
+                        Intent i = new Intent(loading_screen.this,MainActivity.class);
+                        startActivity(i);
+                        finish();
+
+                    }
+                }, 2000);
+
+
+    }
+}
